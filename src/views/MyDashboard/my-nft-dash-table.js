63fb07ea3e973bb7638ca21aad682fdf3edb1efc
Sigala <cryptobet95@gmail.com>
2021-06-22 23:51:15 -0400

    ok

--- src/views/MyDashboard/my-nft-dash-table.js
+++ src/views/MyDashboard/my-nft-dash-table.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Styled from './mydashboard.style';
 import mockData from '../../mockData/mynfts.js';
 import MyNFTCard from '../../components/MyNFTCard';
-
+import { useSelector } from 'react-redux';
 const MyNFTDashTable = () => {
-    console.log(mockData)
+    const address = useSelector(state => state.authUser.address);
     return (
         <Styled.NftDashTable>
             <span>Genisis set</span>
             <div>
-                {mockData.map((item, index) => {
+                {address ? mockData.map((item, index) => {
                     return <MyNFTCard key={index} rarity={item.rarity} name={item.name} description={item.description} />
-                })}
+                }) : <>Please Connect your account</>}
             </div>
         </Styled.NftDashTable>
     )
