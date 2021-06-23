e9b007c5b8daa93eeb835ea42d61f85187b8d5fa
Sigala <cryptobet95@gmail.com>
2021-06-22 23:42:58 -0400

    fixing errors

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { NotificationContainer } from 'react-notifications';
 import Header from '../components/Header';
 import Button from '../components/Button';
